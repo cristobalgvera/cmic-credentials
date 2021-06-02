067c5587776f2ba75f78f7d9f3ef88d521888bc9
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-06-01 23:37:33 -0400

    :sparkles: Added capability to send daily emails
    Also was refactored some code to be more reusable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import {sendEmails} from './services/email.service';
-import {getRegistries, updateRegistries} from './services/registries.service';
+import {
+  sendCredential,
+  sendDailyEmail,
+  sendEmailsOn,
+} from './services/email.service';
 
-function createCredentials() {
-  const registries = getRegistries();
-  const mailedRegistries = sendEmails(registries);
+function sendCredentials() {
+  sendEmailsOn(sendCredential);
+}
 
-  updateRegistries(mailedRegistries);
+function sendDailyEmails() {
+  sendEmailsOn(sendDailyEmail);
 }
--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,6 +1,16 @@
 import {Registry} from '../interfaces/registry.interface';
+import {getRegistries, updateRegistries} from './registries.service';
 
-export const sendEmails = (registries: Registry[]) => {
+export function sendEmailsOn(
+  emailSendFunction: (registries: Registry[]) => Registry[]
+) {
+  const registries = getRegistries();
+  const mailedRegistries = emailSendFunction(registries);
+
+  updateRegistries(mailedRegistries);
+}
+
+export const sendCredential = (registries: Registry[]) => {
   const filteredRegistries = registries.filter(
     ({email, credential, notified}) => !!email && !!credential && !notified
   );
@@ -19,7 +29,8 @@ export const sendEmails = (registries: Registry[]) => {
 
     try {
       MailApp.sendEmail({
-        to: email,
+        // to: email,
+        to: 'd.corcuera01@ufromail.cl',
         subject: '✨ Comenzó C-MIC ✨',
         name: 'NUMIC',
         attachments: [credential.getAs(MimeType.PDF)],
@@ -37,3 +48,40 @@ export const sendEmails = (registries: Registry[]) => {
 
   return mailedRegistries;
 };
+
+export function sendDailyEmail(registries: Registry[]) {
+  const filteredRegistries = registries.filter(
+    ({email, notified}) => !!email && !notified
+  );
+
+  const confirmationHtml = HtmlService.createTemplateFromFile(
+    'app/assets/confirmation.html'
+  );
+  const mailedRegistries = filteredRegistries.map(registry => {
+    const {email, name, id} = registry;
+    const firstName = name.split(' ')[0].trim();
+    confirmationHtml.firstName = firstName;
+    const htmlBody = confirmationHtml.evaluate().getContent();
+
+    console.log(`Sending email to ${email} - ID: ${id}, Name: ${name}`);
+
+    try {
+      MailApp.sendEmail({
+        // to: email,
+        to: 'd.corcuera01@ufromail.cl',
+        subject: 'Segundo día C-MIC ⭐',
+        name: 'NUMIC',
+        htmlBody,
+      });
+
+      registry.notified = true;
+    } catch (error) {
+      console.error(error);
+      registry.notified = false;
+    }
+
+    return registry;
+  });
+
+  return mailedRegistries;
+}
